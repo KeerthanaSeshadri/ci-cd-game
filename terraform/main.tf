35ba2a8a1efabce15ce9b69755a8a5c6121fb829
Keerthana S <keerthanas.23csd@kongu.edu>
2026-02-11 14:45:17 +0530

    Update security group and instance name in Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "game_server" {
   instance_type = "t3.small"
   key_name      = "bbb"
 
-  security_groups = [aws_security_group.game-sg-jenkins.name]
+  security_groups = [aws_security_group.gamenew.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -18,7 +18,7 @@ resource "aws_instance" "game_server" {
               EOF
 
   tags = {
-    Name = "Game-Server"
+    Name = "gamenew"
   }
 }
 
